5e43ebb4048eb16ab51fc6fd3f7e88c7cc126e52
erasels <emanuelbedburdick@yahoo.de>
2022-01-03 20:07:46 +0100

    Fix scryed keyword being unclear

--- WatcherRebalance/src/main/java/WatcherRebalance/WatcherRebalance.java
+++ WatcherRebalance/src/main/java/WatcherRebalance/WatcherRebalance.java
@@ -102,7 +102,7 @@ public class WatcherRebalance implements
         BaseMod.addKeyword("Divinity", new String[]{"divinity"},"Upon entering this stance, gain [W] [W] [W] and draw 2 cards. Switch back to your previous #yStance at the start of your next turn.");
         BaseMod.addKeyword("Stance", new String[]{"stance", "stances"},"You can only have one stance at a time. NL Whenever you switch between them, gain #b1 #yMantra.");
 
-        BaseMod.addKeyword("Scry", new String[]{"scry", "scryed"},"Look at the top X cards of your draw pile. You may discard any of them.");
+        BaseMod.addKeyword("Scryed", new String[]{"scryed"},"The effect triggers when the card is discarded via #yScry.");
 
         BaseMod.addKeyword("watcherrebalance:" ,"Reshuffle", new String[]{"reshuffle"},"When played, shuffle this card into your draw pile.");
     }
